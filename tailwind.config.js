d36815061d3e2b1d116f2b7ab0dd002a362aee95
erkinov97 <erkinov997@gmail.com>
2024-03-10 02:36:05 +0500

    feat: added custom ui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        current: "currentColor",
+        transparent: "transparent",
+        ligth: "#ffffff",
+        dark: "#000000",
+        primary: "#326CF9",
+        gray: "#707070",
+      },
+    },
   },
   plugins: [],
-}
-
+};
